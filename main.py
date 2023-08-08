263b7dbbb7502853db19acf08d3579db6df23800
Xt_One <mansur20071903@gmail.com>
2023-08-08 16:29:32 +0500

    - Сортировка пузырьком(решение от Xt_One)

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from random import randint
+
+
 def main():
-    pass  # вставляй сюда код
+    a = [randint(1, 100) for _ in range(100)]
+    print(a)
+    for i in range(len(a) - 1):
+        for k in range(len(a) - 1 - i):
+            if a[k] > a[k + 1]:
+                a[k], a[k + 1] = a[k + 1], a[k]
+    print(a)
 
 
 if __name__ == "__main__":
