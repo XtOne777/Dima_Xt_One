6645dbd547842516517662a050cad9a907eb9dc7
Xt_One <mansur20071903@gmail.com>
2023-08-08 14:40:51 +0500

    - Решение задачки Xt_One

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ asdas = int()
 
 tuple_example = (1, 2, 3, "21asd")
 dictionary = {1, 2, 3, 4, "asdasd"}
+
+a = input().split(";")
+b = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+for i in a:
+    flag = True
+    for k in i:
+        if k not in b:
+            flag = False
+    if flag:
+        print(i + " - число")
+        continue
+    print(i + " - не число")
