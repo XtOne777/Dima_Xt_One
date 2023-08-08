91730e8afc1a6168da81e95fd2b429539874dff5
Xt_One <mansur20071903@gmail.com>
2023-08-08 16:18:17 +0500

    - Скелет функции

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-# Одиночные Типы
-integer = int(6)
-float_example = float()
-string = str()
-boolean = bool()
+def main():
+    pass  # вставляй сюда код
 
-# Списки, сборки
-lol = int()
-asdas = int()
 
-tuple_example = (1, 2, 3, "21asd")
-dictionary = {1, 2, 3, 4, "asdasd"}
-
-a = input().split(";")
-for i in a:
-    if len(list(filter(lambda x: x.isdigit(), list(i)))) == len(i):
-        print(i + " - число")
-        continue
-    print(i + " - не число")
+if __name__ == "__main__":
+    main()
